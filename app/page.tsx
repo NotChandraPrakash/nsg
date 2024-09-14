ecfaf730a2ddab638193ade3f5c91231ab63a82e
Chandra Prakash <chandra.prakash27088@gmail.com>
2024-09-14 19:58:52 +0530

    nsg

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Nsgtrack } from "@/components/component/nsgtrack";
+import { Page2 } from "@/components/component/page2";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Page2/>
+      <Nsgtrack/>
+    </div>
+  );
+}
